51c679764f69a2e4d2e7746ebdaad983c53ebfe7
esthe <ko990727@hmail.com>
2021-08-30 12:06:49 +0900

    commit

--- module, package/do.py
+++ module, package/do.py
@@ -1,2 +1,19 @@
-import my_module
-my_module.나라친구()
+# import my_module
+# # my_module.나라친구()
+# import tour.jeonnam
+# tour.jeonnma.채리()
+
+# from tour import jeonnam as tj
+# tj.채리()
+#
+# from tour.jeonnam import as je
+# je.채리()
+
+# from tour.jeonnam import 채리 as cherry
+# 채리()
+# cherry()
+
+# from tour import *
+# jeonnam.채리()
+# qp = gangwondo.GangwondoPackage()
+# print(qp)
