203ecfdbd87fcbd031e40e7cbad8a9b0d788d93d
esthe <ko990727@hmail.com>
2021-09-27 14:10:51 +0900

    save text(utf-8)

--- file handler/save_text.py
+++ file handler/save_text.py
@@ -1,8 +1,9 @@
-f= open('text.txt', 'w')
+f= open('text.txt', 'w', encoding='utf-8')
 
-f.write('고에스더')
+f.write('고에스더:')
 f.write('초록색')
-f.write('고수돌')
+f.write('\n')
+f.write('고수돌:')
 f.write('하늘색')
 
 f.close()
