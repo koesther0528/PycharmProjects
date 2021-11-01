a2e829936d17b9edf7495598011fcced18b2a88f
esthe <ko990727@hmail.com>
2021-11-01 14:17:17 +0900

    tictactoe_game engine, show board, set , set winner , position_to index, change turn

--- tictactoe/tictactoe_gameengine.py
+++ tictactoe/tictactoe_gameengine.py
@@ -4,20 +4,53 @@ class TictactoeGameEngine:
         self.turn = 'X'
 
     def show_board(self):
-        print(self.board)
+        print('  '.join(self.board[0:3]))
+        print('  '.join(self.board[3:6]))
+        print('  '.join(self.board[6:9]))
+
+        # for i, v in enumerate(self.board):
+        #     print(v+ '  ', end='')
+        #     if i % 3 == 2:
+        #         print()
 
     def set(self, row, col):
-        pass
+        # self.board[(row * 3 - 3) + (col - 1)] = self.turn
+        index = (row - 1) * 3 + (col - 1)
+        self.board[index] = self.turn
+
+    def position_to_index(self, row, col):
+        return (row -1) *3 + (col -1)
 
     def set_winner(self):
-        # 3줄
-        # 3줄 \ /
-        # 무승부: 위의 조건 다 통과, 더 이상 놓을 자리 없음: self.board에 빈칸이 없음: self.board에 '.'가 없음
-        return 'd' # drow: 무승부
-        pass
+        # -줄
+        for row in range(1, 3 + 1):
+            if self.board[self.position_to_index(row,1)] == self.board[self.position_to_index(row,2)] == self.board[self.position_to_index(row,3)] == self.turn:
+                return '-'
+        # |줄
+        for col in range(1, 3 + 1):
+            if self.board[self.position_to_index(1, col)] == self.board[self.position_to_index(2, col)] == self.board[self.position_to_index(3, col)] == self.turn:
+                return '|'
+
+        # /
+        if self.board[self.position_to_index(1,3)] == self.board[self.position_to_index(2,2)] == self.board[self.position_to_index(3,1)] == self.turn:
+            return '/'
+
+        # \
+        if self.board[self.position_to_index(1, 1)] == self.board[self.position_to_index(2, 2)] == self.board[self.position_to_index(3, 3)] == self.turn:
+            return '\\'
+
+        # 끝나는 경우 : 무승부(승자가 없는 상태로 놓을 자리가 없음), 승자 결정(승자가 있음)
+        if self.board[0] != '.' and self.board[1] != '.' and self.board[2] != '.' and \
+            self.board[3] != '.' and self.board[4] != '.' and self.board[5] != '.' and \
+            self.board[6] != '.' and self.board[7] != '.' and self.board[8] != '.':
+            return print('무승부')
 
     def change_turn(self):
-        pass
+        # if self.turn == 'X':
+        #     self.turn = 'O'
+        # else:
+        #     self.turn = 'X'
+        self.turn = 'O' if self.turn == 'X' else 'X'
 
 if __name__ == '__main__':
     game_engine = TictactoeGameEngine()
